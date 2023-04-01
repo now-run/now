12d284e757d27a9aa5058651bc460c466a1cbe8c
Cléber Zavadniak <contato@cleber.solutions>
2023-03-31 22:32:20 -0300

    "base64.{encode|decode}" commands

--- source/now/commands/general.d
+++ source/now/commands/general.d
@@ -3,6 +3,7 @@ module now.commands.general;
 
 import std.algorithm.mutation : stripRight;
 import std.array;
+import std.base64;
 import std.datetime;
 import std.datetime.stopwatch : StopWatch;
 import std.digest.md;
@@ -1118,6 +1119,54 @@ static this()
     commands["uuid.random"] = function (string path, Context context)
     {
         return context.push(randomUUID().to!string);
+    };
+
+    // Encodings
+    commands["base64.encode"] = function (string path, Context context)
+    {
+        foreach (item; context.items)
+        {
+            ubyte[] data;
+            switch (item.type)
+            {
+                case ObjectType.String:
+                    auto s = cast(String)item;
+                    data = cast(ubyte[])(s.toString());
+                    break;
+                case ObjectType.Name:
+                    auto s = cast(NameAtom)item;
+                    data = cast(ubyte[])(s.toString());
+                    break;
+                case ObjectType.Vector:
+                    if (item.typeName == "byte_vector")
+                    {
+                        auto s = cast(ByteVector)item;
+                        data = cast(ubyte[])(s.values);
+                        break;
+                    }
+                    goto default;
+                default:
+                    return context.error(
+                        "Invalid input for " ~ path
+                        ~ ": " ~ item.type.to!string(),
+                        ErrorCode.InvalidArgument,
+                        ""
+                    );
+            }
+            auto result = Base64URL.encode(data);
+            context.push(result.to!string);
+        }
+        return context;
+    };
+    commands["base64.decode"] = function (string path, Context context)
+    {
+        foreach (item; context.items)
+        {
+            auto s = item.toString();
+            ubyte[] result = Base64URL.decode(s);
+            context.push(cast(string)result);
+        }
+        return context;
     };
 
     // Random
