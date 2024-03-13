32e70ade89aaa475630ef923153c5122e8fe9124
Cléber Zavadniak <contato@cleber.solutions>
2024-03-13 09:39:12 -0300

    library example small changes

--- examples/libraries/library.sh
+++ examples/libraries/library.sh
@@ -8,16 +8,16 @@ echo "  arguments: $*" >&2
 while read msg;do
     echo "lib/msg:$msg" >&2
 
-    rpc_op=$(echo $msg | jq -r ".rpc.op")
-    procedure=$(echo $msg | jq -r ".procedure")
-    args=$(echo $msg | jq -r ".args")
-    kwargs=$(echo $msg | jq -r ".kwargs")
+    rpc_op=$(echo $msg | jq -rc ".rpc.op")
+    procedure=$(echo $msg | jq -rc ".procedure")
+    args=$(echo $msg | jq -rc ".args")
+    kwargs=$(echo $msg | jq -rc ".kwargs")
 
     if [[ $procedure == "err" ]];then
         echo '{"rpc":{"op":"error"},"message":"self_inflicted_error"}'
         continue
     elif [[ $procedure == "test_calls" ]];then
-        call="{\"rpc\":{\"op\":\"call\"},\"procedure\":\"sum\",\"args\":[1,2],\"kwargs\":{}, \"user_data\":null}"
+        call="{\"rpc\":{\"op\":\"call\"},\"procedure\":\"sum\",\"args\":$args,\"kwargs\":{}, \"user_data\":null}"
         echo $call
         read response
         response_op=$(echo $response | jq -r ".rpc.op")
@@ -25,10 +25,12 @@ while read msg;do
         response_result=$(echo $response | jq -r ".result")
         echo "response.result:$response_result" >&2
         # Fallthrough to respond initial call, that's still open.
+        response="{\"rpc\":{\"op\":\"return\"},\"result\":$response_result}"
+        echo $response
+        continue
     fi
 
     response="{\"rpc\":{\"op\":\"return\"},\"result\":[$args,$kwargs]}"
-
     echo "lib/response:$response" >&2
     echo $response
 done
