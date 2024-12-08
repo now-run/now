2cb7465a47cf6e0f8357d48036801734a0362ac1
Cléber Zavadniak <contato@cleber.solutions>
2024-12-07 22:00:36 -0300

    SystemProcess::wait will exhaust `next` if there is an InputStream

--- source/now/system_command/methods.d
+++ source/now/system_command/methods.d
@@ -9,6 +9,8 @@ static this()
 {
     systemProcessMethods["get"] = function (Item target, string path, Input input, Output output)
     {
+        // TODO: get rid of this, probably.
+
         auto process = cast(SystemProcess)target;
 
         if (input.args.length == 0)
@@ -23,17 +25,17 @@ static this()
         switch (argument)
         {
             case "is_running":
-                output.items ~= new Boolean(process.isRunning);
+                output.push(process.isRunning);
                 break;
             case "cmdline":
                 output.items ~= process.cmdline.map!(x => new String(x)).array;
                 break;
             case "pid":
-                output.items ~= new Integer(process.pid.processID());
+                output.push(process.pid.processID());
                 break;
             case "return_code":
                 process.wait();
-                output.items ~= new Integer(process.returnCode);
+                output.push(process.returnCode);
                 break;
             default:
                 break;
@@ -45,8 +47,20 @@ static this()
     {
         auto process = cast(SystemProcess)target;
 
+        log("Process inputStream is ", process.inputStream);
+        if  (process.inputStream !is null)
+        {
+            auto nextOutput = new Output;
+            while (process.inputStream !is null)
+            {
+                log("Process is running ", process.inputStream);
+                process.next(input.escopo, nextOutput);
+            }
+        }
+
+        log("Waiting for ", process);
         process.wait();
-        output.items ~= new Integer(process.returnCode);
+        output.push(process.returnCode);
         return ExitCode.Success;
     };
     systemProcessMethods["success"] = function (Item target, string path, Input input, Output output)
@@ -54,7 +68,7 @@ static this()
         auto process = cast(SystemProcess)target;
 
         process.wait();
-        output.items ~= new Boolean(process.returnCode == 0);
+        output.push(process.returnCode == 0);
         return ExitCode.Success;
     };
     systemProcessMethods["check"] = function (Item target, string path, Input input, Output output)
