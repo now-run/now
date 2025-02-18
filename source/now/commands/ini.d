07c778308e8873aa8d86e4061c1b5d8e30a47032
Cléber Zavadniak <contato+git@cleber.solutions>
2025-02-18 19:14:37 -0300

    ini.decode -- (eol = $eol) and improvements on stripping keys/values

--- source/now/commands/ini.d
+++ source/now/commands/ini.d
@@ -1,6 +1,6 @@
 module now.commands.ini;
 
-import std.algorithm.mutation : stripRight;
+import std.string : strip, stripRight;
 
 import now;
 import now.commands;
@@ -9,6 +9,8 @@ import now.parser;
 
 class IniParser : Parser
 {
+    char defaultEol = ';';
+
     this(string code)
     {
         super(code);
@@ -52,10 +54,11 @@ class IniParser : Parser
                 key ~= consumeChar();
             }
             if (eof) break;
-            key = key.stripRight(' ');
+            log("ini.key=", key);
+            key = key.strip();
             consumeChar();  // '='
             consumeWhitespaces();
-            auto closer = ';';
+            auto closer = this.defaultEol;
             bool isEnclosed = false;
             if (currentChar.among('"', '\''))
             {
@@ -65,7 +68,7 @@ class IniParser : Parser
             auto value = consume_string(closer);
             if (!isEnclosed)
             {
-                value = value.stripRight(' ');
+                value = value.stripRight();
             }
             dict[key] = new String(value);
             // Whatever comes next, we ignore:
@@ -81,7 +84,15 @@ void loadIniCommands(CommandsMap commands)
     commands["ini.decode"] = function(string path, Input input, Output output)
     {
         string content = input.pop!string();
+
         auto parser = new IniParser(content);
+        if (auto closerRef = ("eol" in input.kwargs))
+        {
+            auto closer = cast(String)(*closerRef);
+            parser.defaultEol = closer.toString[0];
+            log("IniParser.defaultEol:", parser.defaultEol);
+        }
+
         output.push(parser.run());
         return ExitCode.Success;
     };
